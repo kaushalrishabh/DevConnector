ef9fdeb08b6873c8eca3134c6382a068d4c8a95d
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-07 15:08:05 +0530

    First commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { connect } = require('mongoose');
+const connectDB = require('./config/db');
 
 const app = express() ;
-
+connectDB();
 app.get('/', (req, res)=> res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
