16b0b41300798959299d2281c9ae335a67e78a1c
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-08 15:35:01 +0530

    Backend login, registration and authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,94 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator')
+const config = require('config');
 
 // @Route   GET api/auth
 // @desc    Test route
 // @access  Public
 
-router.get('/', auth,(req, res) => res.send('Auth route'));
+router.get('/', auth, async(req, res) => {
+
+    try
+    {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        res.status(500).send('Server/DB Error');
+    }
+
+});
+
+// @Route   POST api/auth
+// @desc    Authenticate User & get token
+// @access  Public
+
+router.post(
+    '/', 
+    [
+        check('email', 'Email is Invalid')
+        .isEmail(),
+        check( 'password', 'Password is required')
+        .exists()
+    ],
+    async(req, res) => 
+    {
+        const errors = validationResult(req);
+        if(!errors.isEmpty())
+            return res.status(400).json({ errors: errors.array() });
+        
+        const { email, password} = req.body    
+        
+        try
+        {
+            // See if users exists
+            let user = await User.findOne({ email });
+            if(!user)
+            {
+                return res.status(400)
+                .json({ errors: [{msg: 'Invalid Username' }] });
+            }
+            
+            // Check if password is correct
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch)
+            {
+                return res.status(400)
+                .json({ errors: [{msg: 'Invalid Password' }] });
+            }
+            
+
+            // Return JsonWebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        if(err) throw err;
+                        res.json( { token });
+                     }
+            );
+            
+        }
+        catch(err)
+        {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+
+    });
+
 
 module.exports = router;
