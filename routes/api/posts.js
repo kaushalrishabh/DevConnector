82df3dcc82b6d3acf6352c9f2b29b4108dac3792
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-09 14:32:56 +0530

    Implementation of Likes, Unlikes in Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -50,4 +50,139 @@ router.post('/',
 
     }
 );
+
+// @Route   GET api/posts
+// @desc    GET all Post
+// @access  Private
+
+router.get('/', auth, async(req, res)=>{
+    try
+    {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        res.status(500).send(' Server Error');
+    }
+
+});
+
+// @Route   GET api/posts/:id
+// @desc    GET post by ID
+// @access  Private
+
+router.get('/:id', auth, async(req, res)=>{
+    try
+    {
+        const posts = await Post.findById(req.params.id);
+        
+        if(!posts)
+            return res.status(404).send('Post not found');
+        
+        res.json(posts);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        if(err.kind ==='ObjectId')
+            return res.status(404).send('Post not found');
+        
+        res.status(500).send(' Server Error');
+    }
+
+});
+
+// @Route   DELETE api/posts
+// @desc    Delete a Post
+// @access  Private
+
+router.delete('/:id', auth, async(req, res)=>{
+    try
+    {
+        const posts = await Post.findById(req.params.id);
+     
+        // Check User
+
+        if(!posts)
+            return res.status(404).send('Post not found');
+
+        if(posts.user.toString() !== req.user.id)
+            return res.status(401).send('User not Authorized');
+        
+        await posts.remove();
+        
+        res.json({ msg: 'Post Removed'});
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        if(err.kind ==='ObjectId')
+            return res.status(404).send('Post not found');
+     
+        res.status(500).send(' Server Error');
+    }
+
+});
+
+// @Route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+
+router.put('/like/:id', auth, async(req, res) => {
+
+    try
+    {
+        const post = await Post.findById(req.params.id);
+
+        //Check if the post has been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length >0)
+            return res.status(400).send('Post already liked');
+    
+        post.likes.unshift({ user: req.user.id});
+        
+        await post.save();
+        res.json(post.likes);
+    
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @Route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+
+router.put('/unlike/:id', auth, async(req, res) => {
+
+    try
+    {
+        const post = await Post.findById(req.params.id);
+
+        //Check if the post has been liked
+        if(post.likes.filter(like=> like.user.toString() === req.user.id).length === 0)
+            return res.status(400).send('Post has not yet been liked');
+    
+        //Get remove index
+
+        const removeIndex = post.likes.map(like=> like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+
+        await post.save();
+        res.json(post.likes);
+    
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
