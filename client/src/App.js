37f1b25cbb1827cd2e67f2e5bf16191d96671024
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-10 12:00:23 +0530

    JSX implentation on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {fragment} from 'react';
+import Navbar from './components/layouts/navbar';
+import Landing from './components/layouts/landing';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <fragment> 
+      <Navbar />
+      <Landing />
+  </fragment>
+  
 
+)
 export default App;
