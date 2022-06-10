55c5cb62428f499f7537afde111b59c7bc928697
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-10 23:26:34 +0530

    Store and Reducer implentation

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import React, {fragment} from 'react';
+import React, { Fragment } from 'react';
 import Navbar from './components/layouts/navbar';
 import Landing from './components/layouts/landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
+  <Provider store = {store}>
   <Router>
-    <fragment> 
+    <Fragment> 
       <Navbar />
       <Routes>
         <Route exact path = '/' element={ <Landing/> } /> 
         <Route exact path ='/login' element = {<Login />} />
         <Route exact path ='/register' element = {<Register />} />
       </Routes>
-    </fragment>
+    </Fragment>
   </Router>
-
+  </Provider>
 )
 export default App;
