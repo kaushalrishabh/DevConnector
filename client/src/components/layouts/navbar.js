b0f504b665bf81cf458d97b23d0763c74add1d13
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-10 12:01:58 +0530

    Navbar and Landing page implementation

--- client/src/components/layouts/navbar.js
+++ client/src/components/layouts/navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <nav class="navbar bg-dark">
+    <nav className="navbar bg-dark">
         <h1>
-            <a href="index.html"><i class="fas fa-code"></i> DevConnector</a>
+            <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
         </h1>
         <ul>
             <li><a href="profiles.html">Developers</a></li>
