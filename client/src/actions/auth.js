907fea1940b66b73464bf8e054e88346a71bb02a
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-06-14 22:28:15 +0530

    Logout functionality implementation

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED, 
     AUTH_ERROR,
     LOGIN_FAIL,
-    LOGIN_SUCCESS 
+    LOGIN_SUCCESS,
+    LOGOUT
     } from './types';
 
 import setAuthToken from '../utils/setAuthToken';
@@ -107,4 +108,9 @@ export const login = (email, password) => async dispatch =>
             type: LOGIN_FAIL
         });
     }
+};
+
+// Logout / Clear profiles
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 }
